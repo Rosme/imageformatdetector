a5d0a355a8ec3991a9c8fa049fc0b01e4337e214
Jean-Sébastien Fauteux <jeanseb.fauteux@gmail.com>
2014-08-03 22:36:39 -0400

    PNG detector works correctly

--- src/detector.cpp
+++ src/detector.cpp
@@ -1,5 +1,6 @@
 #include "detector.hpp"
 #include "format.hpp"
+#include "image.hpp"
 
 #include <iostream>
 
@@ -9,6 +10,15 @@ void JPGDetector::detect(const std::string& image, Format& format) {
 
 void PNGDetector::detect(const std::string& image, Format& format) {
 	std::cout << "Testing for PNG\n";
+
+	//PNG is define by an 8 byte signature
+	// 89 50 4E 47 0D 0A 1A 0A
+	// See http://en.wikipedia.org/wiki/Portable_Network_Graphics#Technical_details
+	std::string signature = image.substr(0, 8);
+	if (signature == "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a") {
+		std::cout << "PNG 8-Byte Signature is valid. Image file is PNG.\n";
+		format = Image::Formats["png"];
+	}
 }
 
 void BMPDetector::detect(const std::string& image, Format& format) {
