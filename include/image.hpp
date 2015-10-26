54e0fa69a7ce6342bd2c3ec0004bf3e6583b06f2
Jean-Sébastien Fauteux <jeanseb.fauteux@gmail.com>
2015-10-26 11:02:16 -0400

    Added license to files

--- include/image.hpp
+++ include/image.hpp
@@ -1,3 +1,25 @@
+/*
+* Copyright (c) 2015 Jean-Sébastien Fauteux
+*
+* This software is provided 'as-is', without any express or implied warranty.
+* In no event will the authors be held liable for any damages arising from
+* the use of this software.
+*
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it freely,
+* subject to the following restrictions:
+*
+* 1. The origin of this software must not be misrepresented; you must not claim
+*    that you wrote the original software. If you use this software in a product,
+*    an acknowledgment in the product documentation would be appreciated but is
+*    not required.
+*
+* 2. Altered source versions must be plainly marked as such, and must not be
+*    misrepresented as being the original software.
+*
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
 #pragma once
 
 #include <string>
@@ -18,18 +40,18 @@ typedef std::unique_ptr<Detector> DetectorPtr;
 class Image {
 public:
 	static
-#ifdef _MSC_VER	
+#ifdef _MSC_VER
 	std::unordered_map<std::string, Format>
-#else	
+#else
 	std::map<std::string, Format>
-#endif	
+#endif
 	Formats;
 	static void loadFormats();
 
 public:
 	Image();
 	explicit Image(const std::string& file);
-	
+
 	void load(const std::string& file = "");
 	const Format& expectedFormat() const;
 	const Format& realFormat() const;
@@ -49,7 +71,7 @@ private:
 	DetectorPtr mDetector;
 
 #ifdef _MSC_VER
-	std::unordered_map<ImageFormat, std::function<DetectorPtr()>> 
+	std::unordered_map<ImageFormat, std::function<DetectorPtr()>>
 #else
 	std::map<ImageFormat, std::function<DetectorPtr()>>
 #endif
